f48a00d79e7d43bacbd62c1147eb3cc1c9da1ae9
Toms <toms.bi@inbox.lv>
2020-09-18 22:51:17 +0000

    TASK4.

--- 17-09/index.php
+++ 17-09/index.php
@@ -5,12 +5,22 @@ class Car
     private string $name;
     private string $licensePlate;
     private int $fuelTankCapacity;
+    private int $password;
+    private float $fuelUsage;
 
-    public function __construct(string $name, string $licensePlate, int $fuelTankCapacity)
+    public function __construct(
+        string $name,
+        string $licensePlate,
+        int $fuelTankCapacity,
+        int $password,
+    float $fuelUsage
+    )
     {
         $this->name = $name;
         $this->licensePlate = $licensePlate;
         $this->fuelTankCapacity = $fuelTankCapacity;
+        $this->password = $password;
+        $this->fuelUsage = $fuelUsage;
     }
 
     public function getName(): string
@@ -30,73 +40,63 @@ class Car
         return $this->fuelTankCapacity;
     }
 
+    public function getPassword(): int
+    {
+        return $this->password;
+    }
+
+    public function getFuelUsage(): float
+    {
+        return $this->fuelUsage;
+    }
+
 }
 
 
-$audi = new Car("Audi", "JK 5561", 80);
-$volvo = new Car("Volvo", "OL 1141", 100);
-$bmw = new Car("BMW", "ET 1234", 60);
+$audi = new Car("Audi", "JK 5561", 80, 111, 5.5);
+$volvo = new Car("Volvo", "OL 1141", 100, 222, 7.4);
+$bmw = new Car("BMW", "ET 1234", 60, 333, 6.3);
+
 
 $usersInput = (integer)readline("Chose car (1/2/3): ");
 $usersPassword = (integer)readline("Enter password: ");
 
-$attempts = 1;
-while ($attempts < 3 || $usersPassword === 1111) {
-    if ($usersPassword === 1111) {
-
-        $litersPerKmAudi = (5 / 100) * 10;
-        $distance = 1;
-        $fuelAudi = $audi->getFuelTankCapacity();
-
-        while ($distance <= 10) {
-            echo $audi->getName() . ", " . "License plate: " . $audi->getLicensePlate() . PHP_EOL;
-            echo "Fuel left in tank: " . number_format($fuelAudi - $litersPerKmAudi, 2) . ", Distance: " . $distance . "km" . PHP_EOL;
-            $fuelAudi = $fuelAudi - $litersPerKmAudi;
-            $distance++;
-        }
-        break;
-    } else {
-        readline("Try again! ");
-
+if($usersInput === 1 && $usersPassword === 111) {
+    $fuel = $audi->getFuelTankCapacity();
+    $distance = 0;
+    while ($fuel >= 0) {
+        echo 'Distance :' . $distance . 'km ';
+        echo 'Brand: ' . $audi->getName() . ' ';
+        echo 'License plate: ' . $audi->getLicensePlate() . ' ';
+        echo 'Fuel: ' . $fuel . PHP_EOL;
+        $distance += 10;
+        $fuel -= ($audi->getFuelUsage() / 100) * 10;
+        sleep(1);
     }
-    $attempts++;
-}
-
-while ($attempts < 3 || $usersPassword === 2222) {
-    if ($usersPassword === 2222) {
-        $litersPerKmVolvo = (7 / 100) * 10;
-        $distance = 1;
-        $fuelVolvo = $volvo->getFuelTankCapacity();
-
-        while ($distance <= 10) {
-
-            echo $volvo->getName() . ", " . "License plate: " . $volvo->getLicensePlate() . PHP_EOL;
-            echo "Fuel left in tank: " . number_format($fuelVolvo - $litersPerKmVolvo, 2) . ", Distance: " . $distance . "km" . PHP_EOL;
-            $fuelVolvo = $fuelVolvo - $litersPerKmVolvo;
-            $distance++;
-        }
-    } else {
-        readline("Try again! ");
+} elseif($usersInput === 2 && $usersPassword === 222) {
+    $fuel = $volvo->getFuelTankCapacity();
+    $distance = 0;
+    while ($fuel >= 0) {
+        echo 'Distance :' . $distance . 'km ';
+        echo 'Brand: ' . $volvo->getName() . ' ';
+        echo 'License plate: ' . $volvo->getLicensePlate() . ' ';
+        echo 'Fuel: ' . $fuel . PHP_EOL;
+        $distance += 10;
+        $fuel -= ($volvo->getFuelUsage() / 100) * 10;
+        sleep(1);
     }
-    $attempts++;
-}
-
-while ($attempts < 3 || $usersPassword === 3333) {
-    if ($usersPassword === 3333) {
-        $litersPerKmBmw = (10 / 100) * 10;
-        $distance = 1;
-        $fuelBmw = $bmw->getFuelTankCapacity();
-
-
-        while ($distance <= 10) {
-
-            echo $bmw->getName() . ", " . "License plate: " . $bmw->getLicensePlate() . PHP_EOL;
-            echo "Fuel left in tank: " . number_format($fuelBmw - $litersPerKmBmw, 2) . ", Distance: " . $distance . "km" . PHP_EOL;
-            $fuelBmw = $fuelBmw - $litersPerKmBmw;
-            $distance++;
-        }
-    } else {
-        readline("Try again! ");
+} elseif($usersInput === 3 &&$usersPassword === 333) {
+    $fuel = $bmw->getFuelTankCapacity();
+    $distance = 0;
+    while ($fuel >= 0) {
+        echo 'Distance :' . $distance . 'km ';
+        echo 'Brand: ' . $bmw->getName() . ' ';
+        echo 'License plate: ' . $bmw->getLicensePlate() . ' ';
+        echo 'Fuel: ' . $fuel . PHP_EOL;
+        $distance += 10;
+        $fuel -= ($bmw->getFuelUsage() / 100) * 10;
+        sleep(1);
     }
-    $attempts++;
+}else{
+    echo "error" . PHP_EOL;
 }
